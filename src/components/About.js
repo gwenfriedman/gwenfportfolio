2018b48376f101f55ad9fd3bc80f7c5d6110c102
Gwen Friedman <gwenfriedman@gwens-air.lan>
2020-09-28 18:13:54 -0400

    updated links

--- src/components/About.js
+++ src/components/About.js
@@ -18,14 +18,14 @@ class About extends React.Component {
                     and interaction design. In my free time I like to sew, draw, play with my dog,
                     and work on my American Sign Language skills.</h4>
                 <div className={'light-blue'}>
-                    <h5 className={'new-thing'}>
+                    <a className={'new-thing'} target="_blank" href={'https://apps.apple.com/us/app/id1531592573'}>
                         Howlerr, an app that generates a theme song for your dog is available now in the App Store.
-                    </h5>
+                    </a>
                 </div>
                 <a href={resume} target="_blank" className={'resume'}> Resume </a>
-                <a href={'https://www.instagram.com/art.by.gwen/'}> Instagram </a>
-                <a href={'https://github.com/gwenfriedman'}> Github </a>
-                <a href={'https://www.linkedin.com/in/gwendolyn-friedman/'}> LinkedIn </a>
+                <a target="_blank" href={'https://www.instagram.com/art.by.gwen/'}> Instagram </a>
+                <a target="_blank" href={'https://github.com/gwenfriedman'}> Github </a>
+                <a target="_blank" href={'https://www.linkedin.com/in/gwendolyn-friedman/'}> LinkedIn </a>
                 <h5> Contact me at gwenrfriedman@gmail.com </h5>
             </div>
         </div>
